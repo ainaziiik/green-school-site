5c67c395e0658f379c5a17a7a19a6b282227870f
ainaziiik <166890029+ainaziiik@users.noreply.github.com>
2026-05-20 14:59:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,48 @@ st.markdown("""
     padding-top: 2rem;
 }
 
+/* NAVBAR */
+.navbar {
+    background: rgba(255,255,255,0.18);
+    backdrop-filter: blur(14px);
+
+    border-radius: 20px;
+    padding: 18px 30px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-bottom: 25px;
+
+    border: 1px solid rgba(255,255,255,0.25);
+
+    box-shadow: 0 8px 32px rgba(0,0,0,0.05);
+}
+
+.logo {
+    font-size: 26px;
+    font-weight: 700;
+    color: #2F3E34;
+}
+
+.menu {
+    display: flex;
+    gap: 30px;
+}
+
+.menu-item {
+    color: #4F5E53;
+    font-size: 16px;
+    text-decoration: none;
+    transition: 0.3s;
+}
+
+.menu-item:hover {
+    color: #7FA98F;
+}
+
+
 /* HERO */
 .hero {
     background: rgba(255,255,255,0.25);
@@ -80,18 +122,47 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-# ---------- HERO ----------
+# ---------- NAVBAR ----------
 st.markdown("""
-<div class="hero">
-    <div class="title">
-        Green Future School
+<div class="navbar">
+
+    <div class="logo">
+        Green Future
     </div>
 
-    <div class="subtitle">
-        Modern education for a new generation
+    <div class="menu">
+        <a class="menu-item">Home</a>
+        <a class="menu-item">About</a>
+        <a class="menu-item">Teachers</a>
+        <a class="menu-item">Gallery</a>
+        <a class="menu-item">Contacts</a>
     </div>
+
 </div>
 """, unsafe_allow_html=True)
+
+
+# ---------- HERO ----------
+left, right = st.columns([1.2, 1])
+
+with left:
+    st.markdown("""
+    <div class="hero">
+        <div class="title">
+            Green Future School
+        </div>
+
+        <div class="subtitle">
+            Modern education for a new generation
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+
+with right:
+    st.image(
+        "https://images.unsplash.com/photo-1523050854058-8df90110c9f1",
+        use_container_width=True
+    )
 
 # ---------- CARDS ----------
 col1, col2, col3 = st.columns(3)
